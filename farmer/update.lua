7c2e51b2ebafbcf5924f416a4f9f868e39c861fa
Yarrak Obama <Fr4cK5.gg@gmail.com>
2024-04-29 07:59:25 +0200

    Update

--- farmer/update.lua
+++ farmer/update.lua
@@ -1,18 +1,16 @@
-local url = "https://raw.githubusercontent.com/Fr4cK5/computer-craft/master/farmer/farming.lua"
-local filename = "farming.lua"
+URL = "https://raw.githubusercontent.com/Fr4cK5/computer-craft/master/farmer/farming.lua"
+FileName = "farming.lua"
 
-shell.run("wget " .. url .. " " .. filename)
+function DownloadFile(url, filename)
+	local req = http.get(url)
+	local data = req.readAll()
 
--- function DownloadFile(url, filename)
--- 	local req = http.get(url)
--- 	local data = req.readAll()
---
--- 	if fs.exists(filename) then
--- 		fs.delete(filename)
--- 	end
--- 	local file = fs.open(filename, "w")
--- 	file.write(data)
--- 	file.close()
--- end
---
--- DownloadFile(url, filename)
+	if fs.exists(filename) then
+		fs.delete(filename)
+	end
+	local file = fs.open(filename, "w")
+	file.write(data)
+	file.close()
+end
+
+DownloadFile(URL, FileName)
